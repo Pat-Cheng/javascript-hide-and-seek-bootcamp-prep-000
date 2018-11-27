dc04ffcf23b7249d6c6f1cec39fa949ed9af592d
Patrick Cheng <pat.rick.cheng@hotmail.com>
2018-11-27 20:43:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById("nested").querySelector('.target')
+}
+
+function increaseRankBy(n){
+  var list = document.querySelectorAll('.ranked-list li')
+  
+  for(var x=0;x<list.length;x++){
+    list[x].innerHTML = (parseInt(list[x].innerHTML) + parseInt(n)).toString();
+  }
+}
+
+function deepestChild(){
+  var list = document.querySelector('#grand-node')
+  return list.querySelectorAll('div')
+  //.querySelectorAll('div')
+  while(list[0].length){
+    list = list[0]
+  }
+  return list.innerHTML
+}
+
